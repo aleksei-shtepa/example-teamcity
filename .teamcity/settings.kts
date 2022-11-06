4ae8fc235c678321e8e8425f9bd9503e23312502
shtepa <shtepa@TeamCity>
2022-11-06 22:38:21 +0000

    TeamCity change in 'Netology' project: general settings of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -35,7 +35,7 @@ project {
 object Build : BuildType({
     name = "Build"
 
-    artifactRules = "*.jar"
+    artifactRules = "target/*.jar"
     publishArtifacts = PublishMode.SUCCESSFUL
 
     vcs {
